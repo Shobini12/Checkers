5ee2d54582ce1c50298faa40fd2922a2c52b08d9
DavidScranton <98499686+DavidScranton@users.noreply.github.com>
2022-03-02 16:27:14 +0000

    Added a basic and overloaded constructor to the player class

--- Player.java
+++ Player.java
@@ -1,1 +1,11 @@
-
+public class Player{
+  private String name;
+  private boolean isAI = false;
+  public Player(){
+    name = "AI";
+    isAI = true;
+  }
+  public Player(String n){
+    name = n;
+  }
+}
