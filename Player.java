c71965869a51bd774aa578b2358a6da2243c4a61
DavidScranton <98499686+DavidScranton@users.noreply.github.com>
2022-03-02 16:35:43 +0000

    Started other methods in player

--- Player.java
+++ Player.java
@@ -1,11 +1,24 @@
 public class Player{
   private String name;
   private boolean isAI = false;
+  private String color;
   public Player(){
     name = "AI";
     isAI = true;
+    color = "Red"
   }
-  public Player(String n){
+  public Player(String n,String c){
     name = n;
+  }
+  public String getName(){
+    return name;
+  }
+  public takeTurn(){
+    if(isAI){
+      
+    }
+    else{
+      //check pieces
+    }
   }
 }
