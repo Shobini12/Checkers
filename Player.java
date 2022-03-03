ea43a799f97d19881435a6faf900205b6892bac7
DavidScranton <98499686+DavidScranton@users.noreply.github.com>
2022-03-03 16:11:10 +0000

    Added plan for takeTurn function

--- Player.java
+++ Player.java
@@ -1,7 +1,9 @@
+import java.util.*;
 public class Player{
   private String name;
   private boolean isAI = false;
   private String color;
+  Scanner scan1 = new Scanner(System.in);
   public Player(){
     name = "AI";
     isAI = true;
@@ -9,16 +11,27 @@ public class Player{
   }
   public Player(String n,String c){
     name = n;
+    color = c;
   }
   public String getName(){
     return name;
   }
-  public takeTurn(){
+  public void takeTurn(Checkers[][] gb){
     if(isAI){
-      
+      //check which pieces match the color
+      //check pieces that can move
+      //check pieces that can jump
+      //check piece(s) that can make the most jumps
+      //move that piece, if tied choose at random
+      //make the change to the gameboard
     }
     else{
-      //check pieces
+      //check which pieces match the color
+      //check pieces that can move
+      //check pieces that can jump
+      //print options to move
+      //get user input on the move
+      //make the change to the gameboard
     }
   }
 }
