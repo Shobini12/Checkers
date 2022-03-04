ff3acf058b37494438e5a5de983e335a21f1a5c4
Shobini12 <66881060+Shobini12@users.noreply.github.com>
2022-03-04 15:34:13 +0000

    added checks for not moving backward unless it's a king

--- GameBoard.java
+++ GameBoard.java
@@ -4,6 +4,7 @@ package Checkers;
 public class GameBoard {
   Scanner kb = new Scanner(System.in);
   Checkers[][] dgb= new Checkers[8][8]; //digital gameboard
+  
   public GameBoard(){
     
     
@@ -13,6 +14,8 @@ public class GameBoard {
     boolean firstCheck = false;
     boolean secondCheck = false;
     boolean thirdCheck = false;
+    boolean fourthCheck = false;
+    boolean fifthCheck = false;
     //check that these inputs are valid and doesn't make it go off the board
     System.out.println("Which piece do you want to move? Enter a     letter (A-H) and number (0-7)");
     String from = kb.nextLine();
@@ -22,18 +25,34 @@ public class GameBoard {
     String to = kb.nextLine();
     char toX = to.charAt(0);
     int toY = (int) to.charAt(1);
+    //check if color of piece matches player
+    if(dgb[fromX][fromY].equals(player1.getColor()){
+      firstCheck = true;
+    }
     //checks if the space is open
     if(dgb[toX][toY] == null){ 
-      firstCheck = true;
+      secondCheck = true;
     }
     //check that the piece is moving forward
     if(toY>fromY){
-      secondCheck = true;
+      thirdCheck = true;
+    }
+    //Kings can move backwards
+    if(isKing){
+      thirdCheck = true;
     }
     //check if where you want to move is a diagonal movement
      if((toX == fromX+1 || toX == fromX-1) && (toY == fromY+1 ||      toY == fromY-1)){ 
-      thirdCheck = true;
+      fourthCheck = true;
     }
+    //check for jump
+    if(){
+      fifthCheck = true; 
+    }
+
+    //make sure all checks are true
+    if(firstCheck && secondCheck && thirdCheck && fourthCheck &&)
+    
     
   
 
