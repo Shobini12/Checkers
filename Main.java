69555184cda1c0225bdd72ac06727a78f170a496
Ethan Olderog <75052109+OlderogEthan803@users.noreply.github.com>
2022-03-09 16:33:07 +0000

    main method, created layout.

--- Main.java
+++ Main.java
@@ -4,6 +4,21 @@ public class Main
   public static void main(String[] args)
     {
       KingChecker test = new KingChecker("10", "5");
+      //declare instance variables and setup
+      //ask for user inputs and settings to store in variable.
+      //while !gameWon{
+      //ask player one to make move
+      //checkjump, prompt the user to jump if true.
+      //check if the move is valid
+      //if the move is valid then do, if not then reask.
+      //update GUI
+      //ask player two to make a move
+      //checkjump, prompt the user to jump if true.
+      //check if the move is valid
+      //if the move is valid then do, if no reask.
+      //update the GUI
+      //end of while loop}
+      //display winning screen.
     }
 }
 
