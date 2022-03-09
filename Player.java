b525aca5e1554889f872d0c470cdfedf0f06b31f
David Scranton <98499686+DavidScranton@users.noreply.github.com>
2022-03-09 16:24:21 +0000

    Worked more on AI

--- Player.java
+++ Player.java
@@ -4,21 +4,25 @@ public class Player{
   private boolean isAI = false;
   private String color;
   Scanner scan1 = new Scanner(System.in);
+  //basic default constructor that makes an AI
   public Player(){
     name = "AI";
     isAI = true;
     color = "Red";
   }
+  //Overloaded Constructor that makes a player
   public Player(String n,String c){
     name = n;
     color = c;
   }
+  //Get methods for the player's color and name
   public String getName(){
     return name;
   }
   public String getColor(){
     return color;
   }
+  //has a player take a turn and runs different methods if they are an AI or player
   public void takeTurn(GameBoard gb){
     if(isAI){
       AImove(gb);
@@ -27,34 +31,65 @@ public class Player{
       gb.movePiece(color);
     }
   }
+
+  //AI
   public void AImove(GameBoard gb){
     Checkers[][] gbd = gb.getBoard();
-    for(int i = 0;i<gbd.length;i++){
-      for(int f = 0;f<gbd[0].length;f++){
-        if(i != 1 && i != 0 && f != 1 && f != 0){
-              if(gbd[i-1][f-1] != null && gbd[i-2][f-2] == null){
-                
-              }
+    boolean hasMoved = false;
+    for(int i = gbd.length-1;i>0;i--){
+      if(hasMoved){
+        break;
+      }
+      else{
+        for(int f = gbd[0].length-1;f>0;f--){
+          if(i != 1 && i != 0 && f != 1 && f != 0){
+            if(gbd[i-1][f-1] != null && gbd[i-2][f-2] == null){
+              gbd[i-1][f-1] = null;
+              gbd[i-2][f-2] = gbd[i][f];
+              gbd[i][f] = null;
+              hasMoved = true;
+              break;
             }
-            if(i != 0 && f != 8 && i != 1 && f != 7){
-              if(gbd[i-1][f+1] != null && gbd[i-2][f+2] == null){
-                
-              }
+          }
+          if(i != 0 && f != 8 && i != 1 && f != 7){
+            if(gbd[i-1][f+1] != null && gbd[i-2][f+2] == null){
+              gbd[i-1][f+1] = null;
+              gbd[i-2][f+2] = gbd[i][f];
+              gbd[i][f] = null;
+              hasMoved = true;
+              break;
             }
-        //if is king
+          }
+          //if is king then it can jump backwards too
+          if(gbd[i][f].getKing()){
             if(i != 8 && f != 0 && i != 7 && f != 1){
               if(gbd[i+1][f-1] != null && gbd[i+2][f-2] == null){
-                
+                gbd[i+1][f-1] = null;
+                gbd[i+2][f-2] = gbd[i][f];
+                gbd[i][f] = null;
+                hasMoved = true;
+                break;
               }
             }
             if(i != 8 && f != 8 && i != 7 && f != 7){
               if(gbd[i+1][f+1] != null && gbd[i+2][f+2] == null){
-                
+                gbd[i+1][f+1] = null;
+                gbd[i+2][f+2] = gbd[i][f];
+                gbd[i][f] = null;
+                hasMoved = true;
+                break;
               }
             }
+          }
+        }
       }
     }
+    if(hasMoved){
+      AImove();
+    }
   }
+
+  //First Version of code for AI, keeping as a reference
  /* public void AImove2(GameBoard gb){
     Checkers[][] gbd = gb.getBoard();
     for(int i = 0;i<gbd.length;i++){
