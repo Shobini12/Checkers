55eb76d2149319b0e445b83c82bdc38f3684f829
Janooosz <100604307+Janooosz@users.noreply.github.com>
2022-04-06 10:27:27 -0500

    Update GameBoardGUI.java
    fixed update, added numbers and letters

--- GameBoardGUI.java
+++ GameBoardGUI.java
@@ -46,6 +46,28 @@ public class GameBoardGUI extends JFrame {
 			g.fillRect(0, 0, 750, 750);
 		
 			//draw board
+			int baseCord = 133;
+			int baseCordY = 593;
+			Color myColor2 = new Color(255, 255, 255);
+            g.setColor(myColor2);
+			g.drawString("A", baseCord, 645);
+			g.drawString("B", (baseCord + square), 645);
+			g.drawString("C", (baseCord + square*2), 645);
+			g.drawString("D", (baseCord + square*3), 645);
+			g.drawString("E", (baseCord + square*4), 645);
+			g.drawString("F", (baseCord + square*5), 645);
+			g.drawString("G", (baseCord + square*6), 645);
+			g.drawString("H", (baseCord + square*7), 645);
+			
+			g.drawString("0", 80, baseCordY);
+			g.drawString("1", 80, (baseCordY - square));
+			g.drawString("2", 80, (baseCordY - square*2));
+			g.drawString("3", 80, (baseCordY - square*3));
+			g.drawString("4", 80, (baseCordY - square*4));
+			g.drawString("5", 80, (baseCordY - square*5));
+			g.drawString("6", 80, (baseCordY - square*6));
+			g.drawString("7", 80, (baseCordY - square*7));
+			
 			for(int x=0;x<8;x++){
 				for(int y=0;y<8;y++){
 					g.setColor(Color.black);
@@ -57,7 +79,7 @@ public class GameBoardGUI extends JFrame {
 					}
 				}
 			}
-		
+			
 			//check 2d gb array to determine piece placement
          Checkers[][] checkers = gameboard.getBoard();
          String color;
