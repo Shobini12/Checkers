e5f5108441055ee921ec108349be6d75a02e892f
jolde <ejolderog@gmail.com>
2022-04-08 09:18:11 -0500

    Ignore

--- Main.java
+++ Main.java
@@ -16,30 +16,28 @@ public class Main
       String p1Name = "";
       String p2Name = "";
 
-      System.out.println("(Player 1) What is your name? (leave blank to play as AI): ");
+      System.out.println("(Player 1) What is your name? : ");
       p1Name = sc.nextLine();
       System.out.println("(Player 2) What is your name? (leave blank to play as AI): ");
       p2Name = sc.nextLine();
 
       gameBoard = new GameBoard();
       gui = new GameBoardGUI(gameBoard);
-      if(p1Name.equals("")){
-        p1 = new Player();
-      }
-      else{
         p1 = new Player(p1Name, "Red");
-      }
       if(p2Name.equals("")){
         p2 = new Player();
       }
       else{
-        p2 = new Player(p1Name, "Blue");
+        p2 = new Player(p2Name, "Blue");
       }
       while(gameBoard.checkWin().equals("null")) {
         clearScreen();
         System.out.println("P1 TURN");
         p1.takeTurn(gameBoard);
         clearScreen();
+        if (!gameBoard.checkWin().equals("null")){
+          break;
+        }
         System.out.println("P2 TURN");
         p2.takeTurn(gameBoard);
       }
